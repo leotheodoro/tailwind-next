21ec29df8dfa8e3f048daedc2593c5bdde26aba9
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-09-11 14:36:45 -0300

    💄 navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="flex-items before:h-116 flex gap-3 font-mono text-5xl font-bold text-emerald-600 before:flex before:w-1 before:bg-sky-500">
-        Hello Tailwind
-      </h1>
-      <h2 className="text-3xl font-bold">Hello Tailwind</h2>
-      <h3>Hello Tailwind</h3>
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
