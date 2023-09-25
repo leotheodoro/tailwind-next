cc5d32f79e2c305f6795af0e833ddd12bac6ccae
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-09-25 16:30:11 -0300

    :iphone: sidebar responsivity

--- src/app/components/SettingsTabs/index.tsx
+++ src/app/components/SettingsTabs/index.tsx
@@ -1,61 +1,78 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { TabItem } from './TabItem'
 import { useState } from 'react'
 
 export function SettingsTabs() {
-  const [currentTab, setCurrentTab] = useState('tab-1')
+  const [currentTab, setCurrentTab] = useState('tab1')
 
   return (
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
-      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
-        <TabItem
-          value="tab-1"
-          title="My Details"
-          isSelected={currentTab === 'tab-1'}
-        />
-        <TabItem
-          value="tab-2"
-          title="Profile"
-          isSelected={currentTab === 'tab-2'}
-        />
-        <TabItem
-          value="tab-3"
-          title="Password"
-          isSelected={currentTab === 'tab-3'}
-        />
-        <TabItem
-          value="tab-4"
-          title="Team"
-          isSelected={currentTab === 'tab-4'}
-        />
-        <TabItem
-          value="tab-5"
-          title="Plan"
-          isSelected={currentTab === 'tab-5'}
-        />
-        <TabItem
-          value="tab-6"
-          title="Billing"
-          isSelected={currentTab === 'tab-6'}
-        />
-        <TabItem
-          value="tab-7"
-          title="Notifications"
-          isSelected={currentTab === 'tab-7'}
-        />
-        <TabItem
-          value="tab-8"
-          title="Integrations"
-          isSelected={currentTab === 'tab-8'}
-        />
-        <TabItem
-          value="tab-9"
-          title="API"
-          isSelected={currentTab === 'tab-9'}
-        />
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+            <TabItem
+              value="tab1"
+              title="My details"
+              isSelected={currentTab === 'tab1'}
+            />
+            <TabItem
+              value="tab2"
+              title="Profile"
+              isSelected={currentTab === 'tab2'}
+            />
+            <TabItem
+              value="tab3"
+              title="Password"
+              isSelected={currentTab === 'tab3'}
+            />
+            <TabItem
+              value="tab4"
+              title="Team"
+              isSelected={currentTab === 'tab4'}
+            />
+            <TabItem
+              value="tab5"
+              title="Plan"
+              isSelected={currentTab === 'tab5'}
+            />
+            <TabItem
+              value="tab6"
+              title="Billing"
+              isSelected={currentTab === 'tab6'}
+            />
+            <TabItem
+              value="tab7"
+              title="Email"
+              isSelected={currentTab === 'tab7'}
+            />
+            <TabItem
+              value="tab8"
+              title="Notifications"
+              isSelected={currentTab === 'tab8'}
+            />
+            <TabItem
+              value="tab9"
+              title="Integrations"
+              isSelected={currentTab === 'tab9'}
+            />
+            <TabItem
+              value="tab10"
+              title="API"
+              isSelected={currentTab === 'tab10'}
+            />
+          </Tabs.List>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          className="flex h-0.5 translate-y-1.5 touch-none select-none flex-col bg-zinc-100"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   )
 }
