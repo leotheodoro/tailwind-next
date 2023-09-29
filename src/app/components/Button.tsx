af0453a7b702660308ddfd3c4f3ec02c64af4b0b
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-09-29 15:23:16 -0300

    :construction: dark sidebar

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -11,8 +11,9 @@ const button = tv({
   variants: {
     variant: {
       primary: 'bg-violet-600 text-white hover:bg-violet-700',
-      outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
-      ghost: 'rounded-md px-2 hover:bg-zinc:50 shadow-none',
+      outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50 ',
+      ghost:
+        'rounded-md px-2 hover:bg-zinc:50 shadow-none dark:hover:bg-zinc-800 text-zinc-500 dark:text-zinc-400',
     },
   },
 
